bdc6b4ac5b1ecfad1089c2ebebea2c42100e7bc0
Zakki0925224 <zakki0925224@gmail.com>
2023-10-08 17:19:36 +0900

    [Fix]: Fixed html

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::env;
 use crate::crawler::LinkExtractor;
 
 use actix_cors::Cors;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use log::{error, info};
 use reqwest::ClientBuilder;
 use serde::Deserialize;
+use serde::Serialize;
 use url::Url;
 
 mod cli;
@@ -14,28 +15,36 @@ mod crawler;
 mod test;
 
 #[derive(Deserialize)]
-struct Index {
+struct Request {
     url: String,
     page_cnt: usize,
 }
 
+#[derive(Serialize)]
+struct RequestResult {
+    links: Vec<String>,
+}
+
 #[get("/request")]
-async fn request(index: web::Query<Index>) -> impl Responder {
+async fn request(index: web::Query<Request>) -> HttpResponse {
     // TODO extractor not must be async method?
     let url = Url::parse(&index.url).unwrap();
     let client = ClientBuilder::new().build().unwrap();
     let link_extractor = LinkExtractor::new(client);
     match link_extractor.get_links(url).await {
         Ok(links) => {
-            for l in links {
+            for l in links.iter() {
                 println!("{}", l);
             }
+            HttpResponse::Ok().json(RequestResult {
+                links: links.iter().map(|l| l.to_string()).collect(),
+            })
         }
-        Err(err) => error!("{}", err),
+        Err(err) => {
+            error!("{}", err);
+            HttpResponse::Ok().json(RequestResult { links: vec![] })
+        }
     }
-
-    // TODO: return json
-    format!("url: {}, count: {}", index.url, index.page_cnt)
 }
 
 #[actix_web::main]
@@ -45,9 +54,10 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         let cors = Cors::default().allow_any_origin();
+
         App::new()
-            //.service(actix_files::Files::new("/", "./public").index_file("index.html"))
             .service(request)
+            .service(actix_files::Files::new("/", "./public").index_file("index.html"))
             .wrap(cors)
     })
     .bind("127.0.0.1:8080")?
